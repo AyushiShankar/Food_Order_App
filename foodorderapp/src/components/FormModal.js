0468bdc0ac919d7fb89f7eec2481eb3202a09190
Ayushi Mishra <mishrayu1998@gmail.com>
2026-02-14 22:56:12 +0530

    New Component

--- foodorderapp/src/components/FormModal.js
+++ foodorderapp/src/components/FormModal.js
@@ -1,5 +1,6 @@
 import { forwardRef, useImperativeHandle, useState } from "react";
 import UserForm from "./UserForm";
+import Backbtn from "../images/backBtn";
 import Button from "./Button";
 
 const FormModal = forwardRef(function FormModal({ onClose }, ref) {
@@ -18,15 +19,18 @@ const FormModal = forwardRef(function FormModal({ onClose }, ref) {
 
   return (
     <div className="modal-backdrop">
-      <dialog className="control">
+      <div className="control cart">
         <div className="modal-actions">
+          <Button className="text-button">
+            <img src={Backbtn} alt="backBtn" />
+          </Button>
           <h2>Please fill the details:</h2>
           <Button className="text-button" onClick={onClose} variant="">
             ✖
           </Button>
         </div>
         <UserForm onClose={onClose} />
-      </dialog>
+      </div>
     </div>
   );
 });
