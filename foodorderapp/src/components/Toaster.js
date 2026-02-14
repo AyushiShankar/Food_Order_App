145da721ed8fc23db5e11cca14316695416c79e6
Ayushi Mishra <mishrayu1998@gmail.com>
2026-02-15 00:49:33 +0530

    Toaster mesage

--- foodorderapp/src/components/Toaster.js
+++ foodorderapp/src/components/Toaster.js
@@ -1,8 +1,8 @@
 export default function Toaster() {
   return (
     <div className="toast">
+      <img src="/images/partPopper.svg" alt="clbImg" />
       <p>
-        <img src="images/partyPopper.svg" alt="clbImg" />
         This <span>Valentine's week</span>, you are eligible for{" "}
         <strong>50% discount</strong>
       </p>
